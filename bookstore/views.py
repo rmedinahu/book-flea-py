348d8a82b555ab0ef8b8d9e853c149bb2c78a8e3
Shane Goff <drunknirish@live.com>
2017-04-25 12:03:13 -0600

    fixed views

--- bookstore/views.py
+++ bookstore/views.py
@@ -46,48 +46,10 @@ class DetailItemCategoryView(DetailView):
     model = ItemCategory
     template_name = 'detail_item_category.html'
     fields = ['item', 'category']
-    
+
 class CategoryDetailView(DetailView):
     model= Category
     template_name='category_view.html'
-
-from django.shortcuts import render
-
-from django.views.generic import TemplateView, DetailView, ListView, CreateView, UpdateView
-
-
-from .models import Item, Category
-
-# Create your views here.
-from django.http import HttpResponse
-
-# Shane: use generics...
-class HomeView(TemplateView):
-    template_name='home.html'
-
-class ItemDetail(DetailView):
-	model=Item
-	template_name='item_detail.html'
-
-class ItemCategoryView(CreateView):
-	model = Category
-	template_name = 'item_category.html'
-	fields = ['name','description']
-
-class UpdateBookView(UpdateView):
-    model = Item
-    template_name ='update.html'
-    fields = ['title', 'category', 'price', 'description', 'owner']
-
-class ItemCreateView(CreateView):
-    """ Show a page containing a form for adding a new Item object.
-    url pattern: /item/add name: item_add
-    """
-    model = Item
-    template_name = 'item_create.html'
-    fields = ['title', 'category', 'price', 'description', 'owner']
-
-
 
 class CreateItemCategoryView(CreateView):
     model = ItemCategory
