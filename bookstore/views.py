762a8e6251941f30bf38fbe20c8945648be9d1a9
Shane Goff <drunknirish@live.com>
2017-05-02 12:00:58 -0600

    added these changes to make it nicer

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,75 +1,1 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-from django.views.generic import TemplateView, DetailView, ListView, CreateView, UpdateView
-
-
-from .models import Item, Category, ItemCategory, ItemRequest
-
-# Create your views here.
-from django.http import HttpResponse
-
-# Shane: use generics...
-class HomeView(TemplateView):
-    template_name='home.html'
-
-class ItemDetail(DetailView):
-	model=Item
-	template_name='item_detail.html'
-
-class ItemCategoryView(CreateView):
-	model = Category
-	template_name = 'item_category.html'
-	fields = ['name','description']
-
-class UpdateItemCategoryView(UpdateView):
-    model = Category
-    template_name = 'update_category.html'
-    fields = ['name', 'description']
-
-class UpdateBookView(UpdateView):
-    model = Item
-    template_name ='update.html'
-    fields = ['title', 'category', 'price', 'description', 'owner']
-
-class ItemCreateView(CreateView):
-    """ Show a page containing a form for adding a new Item object.
-    url pattern: /item/add name: item_add
-    """
-    model = Item
-    template_name = 'item_create.html'
-    fields = ['title', 'category', 'price', 'description', 'owner']
-
-class DetailItemCategoryView(DetailView):
-    model = ItemCategory
-    template_name = 'detail_item_category.html'
-    fields = ['item', 'category']
-
-class CategoryDetailView(DetailView):
-    model= Category
-    template_name='category_view.html'
-
-class CreateItemCategoryView(CreateView):
-    model = ItemCategory
-    template_name = 'createitem_category.html'
-    fields = ['item', 'category']
-
-class ItemRequestView(CreateView):
-    model = ItemRequest
-    template_name = 'itemrequestview.html'
-    fields = ['item', 'requestor', 'request_complete']
-
-class UpdateCategoryView(UpdateView):
-    model = Category
-    template_name = 'actually_update_category.html'
-    fields = ['name', 'description']
-class UpdateItemRequestView(UpdateView):
-    template_name = 'update_item_request.html'
-    model = ItemRequest
-    fields = ['item', 'requestor', 'request_complete']
-class DetailItemRequestView(DetailView):
-    model = ItemRequest
-    fields = ['item', 'requestor']
-    template_name = 'detail_item_request.html'
+# -*- coding: utf-8 -from __future__ import unicode_literalsfrom django.shortcuts import renderfrom django.views.generic import TemplateView, DetailView, ListView, CreateView, UpdateViewfrom .models import Item, Category, ItemCategory, ItemRequest# Create your views here.from django.http import HttpResponse# Shane: use generics...class HomeView(TemplateView):    template_name='home.html'class ItemDetail(DetailView):	model=Item	template_name='item_detail.html'class ItemCategoryView(CreateView):	model = Category	template_name = 'item_category.html'	fields = ['name','description']class UpdateItemCategoryView(UpdateView):    model = Category    template_name = 'update_category.html'    fields = ['name', 'description']class UpdateBookView(UpdateView):    model = Item    template_name ='update.html'    fields = ['title', 'category', 'price', 'description', 'owner']class ItemCreateView(CreateView):    """ Show a page containing a form for adding a new Item object.    url pattern: /item/add name: item_add    """    model = Item    template_name = 'item_create.html'    fields = ['title', 'category', 'price', 'description', 'owner']class DetailItemCategoryView(DetailView):    model = ItemCategory    template_name = 'detail_item_category.html'    fields = ['item', 'category']class CategoryDetailView(DetailView):    model= Category    template_name='category_view.html'class CreateItemCategoryView(CreateView):    model = ItemCategory    template_name = 'createitem_category.html'    fields = ['item', 'category']class ItemRequestView(CreateView):    model = ItemRequest    template_name = 'itemrequestview.html'    fields = ['item', 'requestor', 'request_complete']class UpdateCategoryView(UpdateView):    model = Category    template_name = 'actually_update_category.html'    fields = ['name', 'description']class UpdateItemRequestView(UpdateView):    template_name = 'update_item_request.html'    model = ItemRequest    fields = ['item', 'requestor', 'request_complete']class DetailItemRequestView(DetailView):    model = ItemRequest    fields = ['item', 'requestor']    template_name = 'detail_item_request.html'
